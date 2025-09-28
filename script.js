3d77e374f0cee51fb57652bd759bd68203cf4ac9
EskoDrums <eskodrums@gmail.com>
2025-09-28 20:36:08 +0300

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+  
+  if(name && email && message){
+    document.getElementById("response").innerText = "Thank you, " + name + "! Your message has been sent.";
+    document.getElementById("contactForm").reset();
+  } else {
+    document.getElementById("response").innerText = "Please fill out all fields.";
+  }
+});
